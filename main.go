952c60ee452cd9b4f4f3decac255c9deb14d008a
Felipe Larenas <wat.sh1ken@gmail.com>
2024-05-23 15:21:27 -0400

    wip wip wip

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func retrieveConfig() Config {
@@ -56,15 +58,72 @@ func copyFile(src, dst string) error {
 	return err
 }
 
+func deleteFirstLine(tempFileRoute string) error {
+	cmd := exec.Command("sed", "-i", "-e", "1d", tempFileRoute)
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getLastRow(tempFileRoute string) (string, error) {
+	cmd := exec.Command("tail", "-n", "1", tempFileRoute)
+
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(output), nil
+}
+
+func appendToFile(filename, text string) error {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("echo '%s' >> %s", text, filename))
+	return cmd.Run()
+}
+
+func removeNewlines(text string) string {
+	return strings.ReplaceAll(text, "\n", "")
+}
+
 func generateRandomRow(tempFileRoute string, file File) error {
 	fmt.Println("| Generating random row for", file.Name, "in", tempFileRoute)
-	/*
-	 * 1. Open the file
-	 * 2. Delete the first row
-	 * 3. Read last row
-	 * 4. Create new record based on the last row and config
-	 * 5. Write the new record to the file
-	 */
+
+	// First, we delete the first line of the file
+	err := deleteFirstLine(tempFileRoute)
+	if err != nil {
+		return err
+	}
+
+	// Secondly, we get the last line of the file
+	lastRow, err := getLastRow(tempFileRoute)
+	if err != nil {
+		return err
+	}
+
+	// Remove any new lines from the last row
+	lastRow = removeNewlines(lastRow)
+	fmt.Println("| Last row:", lastRow)
+
+	// Then we generate data based on the last row that we just got
+	lastRowColumns := strings.Split(lastRow, file.Separator)
+	newRowData := []string{}
+
+	for index, column := range file.Columns {
+		fmt.Println("| Generating data for", column.Name)
+		newRowData = append(newRowData, lastRowColumns[index])
+	}
+
+	// Finally, we append a new line with randomized data
+	newRow := strings.Join(newRowData, file.Separator)
+	fmt.Println("| New row:", newRow)
+	err = appendToFile(tempFileRoute, newRow)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -74,7 +133,8 @@ func main() {
 
 	// For each station
 	for _, file := range config.Files {
-		fmt.Println("| Processing", file.Name)
+		fmt.Println("|----")
+		fmt.Println("| Initializing", file.Name)
 
 		// Create a temporary copy of the file
 		originalFileRoute := config.DataFolder + file.Name
@@ -109,9 +169,13 @@ func main() {
 			return
 		}
 
+		fmt.Println("|", file.Name, "finished")
+
 		// fmt.Println(file.Name, file.Separator, file.Interval, file.DateConfig.Format, file.DateConfig.Gmt)
 		// for _, column := range file.Columns {
 		// 	fmt.Println(column.Name, column.Type, column.Position)
 		// }
 	}
+
+	fmt.Println("|----")
 }
